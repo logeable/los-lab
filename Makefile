77b93c52de266f40330d5f6ccff83e4c366aedc1
logeable <logeable@gmail.com>
2024-08-27 15:19:22 +0800

    add app to print stack info

--- Makefile
+++ Makefile
@@ -2,4 +2,6 @@ qemu:
 	cd los-user; cargo build --release
 	cargo xtask app asm
 	make -C los qemu
-	
+
+gdb:
+	make -C los gdb
---
+++ los-user/src/bin/3_stack.rs
@@ -0,0 +1,41 @@
+#![no_std]
+#![no_main]
+
+use core::arch::asm;
+
+use los_user::{self, println};
+
+#[no_mangle]
+fn main() -> i32 {
+    foo();
+
+    0
+}
+
+fn foo() {
+    bar()
+}
+
+// use addr2line to get function name and filename
+// addr2line -e <ELF_FILE> -C ADDRESS [ADDRESSES...]
+fn bar() {
+    let mut fp: usize;
+    let mut pc: usize;
+    unsafe {
+        asm!(
+            "mv {fp}, fp",
+            "auipc {pc}, 0",
+            fp = out(reg) fp,
+            pc = out(reg) pc,
+        );
+    }
+
+    println!("pc:{:#x}", pc);
+
+    while fp != 0 {
+        let ra = unsafe { *((fp - 8) as *const u64) as usize };
+        println!("ra:{:#x} fp:{:#x}", ra, fp);
+
+        fp = unsafe { *((fp - 16) as *const u64) as usize };
+    }
+}
